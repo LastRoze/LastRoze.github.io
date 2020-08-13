38ad19bace2080777263ba2cb08d5f8c2332f76d
Last Roze <LastRoze@users.noreply.github.com>
2020-08-14 03:52:25 +0700

    Updates

---
+++ precache-manifest.26ed0d8d4c1525c810e5e1156cdf6fec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9618e96dcc68a1232dad052a636afd26",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "9e59363b215261a30485",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3f69311c81f845587b79",
+    "url": "/home/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "9e59363b215261a30485",
+    "url": "/home/static/js/2.7dc6eb04.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.7dc6eb04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f69311c81f845587b79",
+    "url": "/home/static/js/main.f5d88a4d.chunk.js"
+  },
+  {
+    "revision": "181cc05cf49a935537f1",
+    "url": "/home/static/js/runtime-main.69aee44e.js"
+  },
+  {
+    "revision": "2fa83d88f939a7812df97ed222f85d55",
+    "url": "/home/static/media/resume.2fa83d88.pdf"
+  }
+]);
